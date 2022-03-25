f665d64ba6b2715c2629bac156c57089016533c4
Mohammad Faisal <mohammadfaisal1011@gmail.com>
2022-03-25 19:12:36 +0600

    Update GoogleAuthentication.tsx

--- src/GoogleAuthentication.tsx
+++ src/GoogleAuthentication.tsx
@@ -5,8 +5,7 @@ import GoogleLogin, {
   GoogleLoginResponseOffline,
 } from "react-google-login";
 
-const CLIENT_ID =
-  "453936919472-efu89ef221p6icp18ivtro5lnui0hv9d.apps.googleusercontent.com";
+const CLIENT_ID = "YOUR_CLIENT_ID";
 
 const SCOPE = "https://www.googleapis.com/auth/drive";
 
@@ -23,9 +22,6 @@ export const GoogleAuthentication = () => {
   ) => {
     console.log(response);
     setIsSignedIn(true);
-    // if (response.accessToken !== undefined) {
-    //   console.log(response.accessToken);
-    // }
   };
 
   return (
